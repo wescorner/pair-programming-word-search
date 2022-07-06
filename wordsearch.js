f449a35ab63ba1affde216f51e790c9f07cfec58
Wesley Corner <wescorner@gmail.com>
2022-07-06 23:29:37 +0000

    working word search that finds words in 2d array whether they are vertical, horizontal (forwards and backwards) or diagonal.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (matrix, word) => {
+	let flag1 = false;
+	let flag2 = true;
+	let flag3 = false;
 
-module.exports = wordSearch
+	for (let i = 0; i < matrix[0].length; i++) {
+		for (let j = 0; j < matrix.length; j++) {
+			if (i === j) {
+				if (word[i] !== matrix[i][j]) flag2 = false;
+			}
+		}
+	}
+
+	for (let i = 0; i < matrix[0].length; i++) {
+		if (
+			matrix[i].join("").includes(word) ||
+			matrix[i].reverse().join("").includes(word)
+		)
+			flag1 = true;
+		let verticalword = "";
+		for (let j = 0; j < matrix.length; j++) {
+			verticalword += matrix[j][i];
+		}
+		if (
+			verticalword.includes(word) ||
+			verticalword.split("").reverse().join("").includes(word)
+		)
+			flag3 = true;
+	}
+	return flag1 || flag2 || flag3;
+};
+
+module.exports = wordSearch;
